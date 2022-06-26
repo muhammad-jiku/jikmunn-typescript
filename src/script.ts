1fcf2ea11933f87253b676824fb64086196fbba1
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-26 22:24:20 +0600

    first commit

---
+++ src/script.ts
@@ -0,0 +1,22 @@
+// defined variables
+
+let fullName = "Munira Akhter Jannat Mili";
+let age = 22;
+console.log(fullName, age);
+
+// It will return errors becaause previoust 'fullName' variables were assign as 'string' so it will not receive any other data types except 'string'
+// fullName = 34
+fullName = "Jikmunn";
+// It will return errors becaause previoust 'age' variables were assign as 'number' so it will not receive any other data types except 'number'
+// age = "22";
+age = 23;
+console.log(fullName, age);
+
+// undefined variables
+let xD;
+
+// It will return last declared data type's value because it's intial data-type and value is undefined
+console.log(xD);
+xD = "xD";
+xD = 364 + 164;
+console.log(xD);
