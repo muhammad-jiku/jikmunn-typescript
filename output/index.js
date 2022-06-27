af7ed126568c0603cdddf21322fad66af84fddeb
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-27 12:50:03 +0600

    function signature is added

--- output/index.js
+++ output/index.js
@@ -40,3 +40,38 @@ userDetails(193242, { name: "jiku", age: 23 });
 sayHello({ name: "jikum", age: 53 });
 sayHello({ name: "jikmunn", age: 23 });
 // type aliases closed
+// functions signature added
+let fSig; //void means it doesnot return anything
+fSig = (x, y) => {
+    console.log(x, y);
+};
+fSig("call", 911);
+let fSigg; //'number' means it does return only 'number' data type
+fSigg = (x, y) => {
+    const z = x + y;
+    console.log(z);
+    return z;
+};
+fSigg(199, 911 + 1);
+let calculation; //'number' means it does return only 'number' data type
+calculation = (x, y, z) => {
+    if (z === "add") {
+        return x + y;
+    }
+    else {
+        return x - y;
+    }
+};
+console.log(calculation(199, 911 + 1, "add"));
+console.log(calculation(199, 911 + 1, "minus"));
+// when function takes object as parameters
+let userInfo;
+userInfo = (_id, detail) => {
+    console.log(detail === null || detail === void 0 ? void 0 : detail.name, detail === null || detail === void 0 ? void 0 : detail.age, _id);
+};
+let userInfoo;
+userInfoo = (_id, detail) => {
+    return (detail === null || detail === void 0 ? void 0 : detail.age) - _id;
+};
+userInfo("p-193242", { name: "Munira Jannat", age: 23 });
+console.log(userInfoo(2019339024, { name: "Jiku", age: 24 }));
--- src/index.ts
+++ src/index.ts
@@ -57,3 +57,72 @@ userDetails(193242, { name: "jiku", age: 23 });
 sayHello({ name: "jikum", age: 53 });
 sayHello({ name: "jikmunn", age: 23 });
 // type aliases closed
+
+// functions signature added
+let fSig: (a: string, b: number) => void; //void means it doesnot return anything
+
+fSig = (x: string, y: number) => {
+  console.log(x, y);
+};
+fSig("call", 911);
+
+let fSigg: (a: number, b: number) => number; //'number' means it does return only 'number' data type
+
+fSigg = (x: number, y: number) => {
+  const z = x + y;
+  console.log(z);
+  return z;
+};
+fSigg(199, 911 + 1);
+
+let calculation: (a: number, b: number, c: string) => number; //'number' means it does return only 'number' data type
+
+calculation = (x: number, y: number, z: string) => {
+  if (z === "add") {
+    return x + y;
+  } else {
+    return x - y;
+  }
+};
+console.log(calculation(199, 911 + 1, "add"));
+console.log(calculation(199, 911 + 1, "minus"));
+
+// when function takes object as parameters
+let userInfo: (
+  id: number | string,
+  details: {
+    name: string;
+    age: number;
+  }
+) => void;
+
+userInfo = (
+  _id: number | string,
+  detail: {
+    name: string;
+    age: number;
+  }
+) => {
+  console.log(detail?.name, detail?.age, _id);
+};
+
+let userInfoo: (
+  id: number,
+  details: {
+    name: string;
+    age: number;
+  }
+) => number;
+
+userInfoo = (
+  _id: number,
+  detail: {
+    name: string;
+    age: number;
+  }
+) => {
+  return detail?.age - _id;
+};
+
+userInfo("p-193242", { name: "Munira Jannat", age: 23 });
+console.log(userInfoo(2019339024, { name: "Jiku", age: 24 }));
