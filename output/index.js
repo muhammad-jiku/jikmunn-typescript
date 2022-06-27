b0f6e6cf10b4bc3f27588aa1b2e1a45da8a52d8a
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-27 09:47:23 +0600

    dynamic types addded

---
+++ output/index.js
@@ -0,0 +1,22 @@
+"use strict";
+let x;
+x = 5;
+console.log(x);
+x = "xD";
+console.log(x);
+// array
+const arr = [];
+arr.push(5, "k", true);
+console.log(arr);
+// object
+let obj;
+obj = {
+    name: "Hi there",
+    age: true,
+};
+console.log(obj);
+obj = {
+    name: false,
+    age: 1,
+};
+console.log(obj);
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+let x: any;
+
+x = 5;
+console.log(x);
+x = "xD";
+console.log(x);
+
+// array
+const arr: any[] = [];
+arr.push(5, "k", true);
+console.log(arr);
+
+// object
+let obj: {
+  name: any;
+  age: any;
+};
+
+obj = {
+  name: "Hi there",
+  age: true,
+};
+console.log(obj);
+
+obj = {
+  name: false,
+  age: 1,
+};
+console.log(obj);
