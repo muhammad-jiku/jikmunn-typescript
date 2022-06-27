72990b6d57adbcb275b323dee3c9c726f86f0cc4
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-27 14:16:03 +0600

    access modifier added

--- output/index.js
+++ output/index.js
@@ -1,9 +1,17 @@
 "use strict";
 class Player {
-    constructor(n, a, c) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
+    //   public name: string; // by default it's 'public' modifier it can be accessible and readble
+    //   private age: number; // by 'private' modifier it can not be accessible outside of class
+    //   readonly country: string; // by 'readonly' modifier it can be accessible only for read
+    //   constructor(n: string, a: number, c: string) {
+    // when access modifier is used
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+        // this.name = n;
+        // this.age = a;
+        // this.country = c;
     }
     play() {
         console.log(`${this.name} from ${this.country} is playing`);
@@ -13,6 +21,8 @@ const player1 = new Player("Mash", 31, "Bangladesh");
 const player2 = new Player("Shakib", "27", "Bangladesh");
 console.log(player1, player2);
 player1.name = "Mashrafee";
+player1.age = 33;
+player1.country = "BD";
 console.log(player1 === null || player1 === void 0 ? void 0 : player1.play());
 console.log(player2 === null || player2 === void 0 ? void 0 : player2.play());
 // declaring 'Player' as type of arraay
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 class Player {
-  name: string;
-  age: number;
-  country: string;
+  //   public name: string; // by default it's 'public' modifier it can be accessible and readble
+  //   private age: number; // by 'private' modifier it can not be accessible outside of class
+  //   readonly country: string; // by 'readonly' modifier it can be accessible only for read
 
-  constructor(n: string, a: number, c: string) {
-    this.name = n;
-    this.age = a;
-    this.country = c;
+  //   constructor(n: string, a: number, c: string) {
+  // when access modifier is used
+  constructor(
+    public name: string,
+    private age: number,
+    readonly country: string
+  ) {
+    // this.name = n;
+    // this.age = a;
+    // this.country = c;
   }
 
   play() {
@@ -20,6 +26,8 @@ const player2 = new Player("Shakib", "27", "Bangladesh");
 console.log(player1, player2);
 
 player1.name = "Mashrafee";
+player1.age = 33;
+player1.country = "BD";
 
 console.log(player1?.play());
 console.log(player2?.play());
